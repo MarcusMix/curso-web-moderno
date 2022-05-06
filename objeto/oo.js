5bdfca83903baf90fb90ba999429673ec123f83f
Marcus  Sandi <97994146+MarcusMix@users.noreply.github.com>
2022-05-06 16:54:39 -0300

    object

---
+++ objeto/oo.js
@@ -0,0 +1,15 @@
+// codigo n executável
+
+processamento(valor1, valor2, valor3);
+
+//orientação objeto
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        //...
+    }
+}
+
+objeto.processamento();

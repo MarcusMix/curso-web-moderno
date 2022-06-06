35a27161582cfa961c50e0fb67508ab9fa70b6db
Marcus  Sandi <97994146+MarcusMix@users.noreply.github.com>
2022-06-06 15:11:46 -0300

    revisao

---
+++ dicasparadevs/revisao.js
@@ -0,0 +1,17 @@
+const first = 'marcus'
+const last = 'sandi'
+
+console.log('meu nome é ' +first + " " + last)
+
+console.log(`meu nome é ${first} ${last}`)
+
+const names = 'felipe,marcus,matheus'
+
+console.log(names.split(","))
+
+
+const names2 = ['marcus' , 'joao' , 'julia']
+
+const joao = names2[0]
+
+console.log(joao)
---
+++ dicasparadevs/revisao2.js
@@ -0,0 +1,7 @@
+const names = ['felipe' , 'sara' , 'marcus' , 10 , false]
+
+names.push("pedroo") // adc na ultima
+names.unshift("milena") // adc na primeira
+names.pop()
+
+console.log(names)

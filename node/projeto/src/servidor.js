0e975432487fce7d3b1f3f1f02467a3e23b9e6c7
Marcus  Sandi <97994146+MarcusMix@users.noreply.github.com>
2022-06-12 20:04:50 -0300

    node.js

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -2,9 +2,25 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded( { extended: true }))
 
 app.get('/produtos', (req, res, next) => {
-    res.send({ nome: 'marcus lindo com desconto' , preco: 123.45 }) //converter para JSON
+    res.send(bancoDeDados.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos' , (req,res,next) => {
+    const produto = bancoDeDados.salvarProduto ({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) //JSON
 })
 
 app.listen(porta, () => {

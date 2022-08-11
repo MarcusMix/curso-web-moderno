1cde557f530e2a3bad0e49dca92cb85131a0d2ea
MarcusMix <tradedomarcus@gmail.com>
2022-08-11 14:49:38 -0300

    Update flappybird

--- dom/js/flappy.js
+++ dom/js/flappy.js
@@ -122,15 +122,36 @@ function Passaro(alturaJogo) {
 }
 
 
-const barreiras = new Barreiras(700, 1200, 200, 400);
-const passaro = new Passaro(700);
 
-const areaDoJogo = document.querySelector('[wm-flappy]');
 
-areaDoJogo.appendChild(passaro.elemento);
-barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento));
-setInterval(() => {
-    barreiras.animar();
-    passaro.animar();
 
-},20)
+function Progresso() {
+    this.elemento  = novoElemento('span', 'progresso');
+    this.atualizarPontos = pontos => {
+        this.elemento.innerHTML = pontos;
+    }
+    this.atualizarPontos(0);
+}
+
+
+// const barreiras = new Barreiras(700, 1200, 200, 400);
+// const passaro = new Passaro(700);
+
+// const areaDoJogo = document.querySelector('[wm-flappy]');
+
+// areaDoJogo.appendChild(passaro.elemento);
+// areaDoJogo.appendChild(new Progresso().elemento)
+// barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento));
+// setInterval(() => {
+//     barreiras.animar();
+//     passaro.animar();
+
+// },20)
+
+function FlappyBird() {
+    let pontos = 0;
+
+    const areaDoJogo = document.querySelector('[wm-flappy]');
+    const altura = areaDoJogo.clientHeight;
+    const altura = areaDoJogo.client;
+}

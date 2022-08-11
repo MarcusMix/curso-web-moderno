60824e9403c546ba2ba47a10164a56903cf4393c
MarcusMix <tradedomarcus@gmail.com>
2022-08-11 14:18:14 -0300

    Update flappy.js

--- dom/js/flappy.js
+++ dom/js/flappy.js
@@ -58,5 +58,43 @@ function ParDeBarreiras(altura, abertura, x) {
     this.setX(x);
 }
 
-const b = new ParDeBarreiras(700, 200, 400);
-document.querySelector('[wm-flappy]').appendChild(b.elemento)
+
+//teste
+// const b = new ParDeBarreiras(700, 200, 400);
+// document.querySelector('[wm-flappy]').appendChild(b.elemento)
+
+
+//criar varias barriras
+function Barreiras(altura, largura, abertura, espaco, notificarPonto) {
+    this.pares = [
+        new ParDeBarreiras(altura, abertura, largura),
+        new ParDeBarreiras(altura, abertura, largura + espaco),
+        new ParDeBarreiras(altura, abertura, largura + espaco * 2),
+        new ParDeBarreiras(altura, abertura, largura + espaco * 3),
+    ]
+
+    const deslocamento = 3;
+    this.animar = () => {
+        this.pares.forEach(par => {
+            par.setX(par.getX() - deslocamento);
+
+            //quando elemento sair da área do jogo
+            if(par.getX() < -par.getLargura()) {
+                par.setX(par.getX() + espaco * this.pares.length);
+                par.sortearAbertura();
+            }
+
+            const meio = largura / 2;
+            const cruzouOMeio = par.getX() + deslocamento >= meio
+                && par.getX() < meio;
+                if(cruzouOMeio) notificarPonto();
+        })
+    }
+}
+
+const barreiras = new Barreiras(700, 1200, 200, 400);
+const areaDoJogo = document.querySelector('[wm-flappy]');
+barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento));
+setInterval(() => {
+    barreiras.animar()
+},20)

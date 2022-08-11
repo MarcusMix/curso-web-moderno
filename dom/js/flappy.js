826f3ea20eeeb59714e7da4859a1a82e1dc96144
MarcusMix <tradedomarcus@gmail.com>
2022-08-11 14:56:30 -0300

    Update flappy.js

--- dom/js/flappy.js
+++ dom/js/flappy.js
@@ -153,5 +153,24 @@ function FlappyBird() {
 
     const areaDoJogo = document.querySelector('[wm-flappy]');
     const altura = areaDoJogo.clientHeight;
-    const altura = areaDoJogo.client;
+    const largura = areaDoJogo.clientWidth;
+
+    const progresso = new Progresso();
+    const barreiras = new Barreiras(altura, largura, 200, 400,
+        () => progresso.atualizarPontos(++pontos));
+    const passaro = new Passaro(altura);
+
+    areaDoJogo.appendChild(progresso.elemento);
+    areaDoJogo.appendChild(passaro.elemento);
+    barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento));
+
+    this.start = () => {
+        //loop do jogo
+        const temporizador = setInterval(() => {
+            barreiras.animar();
+            passaro.animar();
+        }, 20)
+    }
 }
+
+new FlappyBird().start();

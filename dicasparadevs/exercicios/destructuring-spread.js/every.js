06e748ded27a57e0f46e439f5a123c903ca64067
Marcus Sandi <tradedomarcus@gmail.com>
2022-09-10 17:50:40 -0300

    Create every.js

---
+++ dicasparadevs/exercicios/destructuring-spread.js/every.js
@@ -0,0 +1,8 @@
+const numeros = [2, 2, 3, 4, 5];
+
+const todosSaoMaioresDoQue2 = numeros.every((item) => {     
+        return item > 2;
+})
+
+console.log(todosSaoMaioresDoQue1)
+console.log(todosSaoMaioresDoQue2)
